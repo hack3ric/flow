bd02d640e513ab80053dfc7a12c60383637ebded
Eric Long <i@hack3r.moe>
2025-01-05 20:47:43 +0800

    xtask: propagate exit status

--- xtask/main.rs
+++ xtask/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use std::process::Command;
+use std::process::{Command, ExitCode};
 
 #[derive(Debug, Parser)]
 enum Cli {
@@ -17,39 +17,38 @@ enum Cli {
   },
 }
 
-fn main() {
-  match Cli::parse() {
-    Cli::Gen => {
-      Command::new(env!("CARGO")).args(["run", "--features=__gen"]).status().unwrap();
-    }
+fn main() -> ExitCode {
+  let status = match Cli::parse() {
+    Cli::Gen => Command::new(env!("CARGO")).args(["run", "--features=__gen"]).status().unwrap(),
 
     #[cfg(target_os = "linux")]
-    Cli::Unshare { _args } => {
-      Command::new(env!("CARGO"))
-        .args(["--config", "target.'cfg(target_os = \"linux\")'.runner = 'unshare -rn'"])
-        .args(std::env::args().skip(2))
-        .status()
-        .unwrap();
-    }
+    Cli::Unshare { _args } => Command::new(env!("CARGO"))
+      .args(["--config", "target.'cfg(target_os = \"linux\")'.runner = 'unshare -rn'"])
+      .args(std::env::args().skip(2))
+      .status()
+      .unwrap(),
 
     #[cfg(not(target_os = "linux"))]
     Cli::Unshare { _args } => {
       eprintln!("Unshare not supported, running tests as current user");
-      Command::new(env!("CARGO")).args(std::env::args().skip(2)).status().unwrap();
+      Command::new(env!("CARGO")).args(std::env::args().skip(2)).status().unwrap()
     }
 
-    Cli::Sudo { _args } => {
-      Command::new(env!("CARGO"))
-        .args([
-          "--config",
-          &format!(
-            "target.'cfg(target_os = \"{}\")'.runner = 'sudo -E'",
-            std::env::consts::OS
-          ),
-        ])
-        .args(std::env::args().skip(2))
-        .status()
-        .unwrap();
-    }
+    Cli::Sudo { _args } => Command::new(env!("CARGO"))
+      .args([
+        "--config",
+        &format!(
+          "target.'cfg(target_os = \"{}\")'.runner = 'sudo -E'",
+          std::env::consts::OS
+        ),
+      ])
+      .args(std::env::args().skip(2))
+      .status()
+      .unwrap(),
+  };
+
+  match status.code() {
+    Some(code) => ExitCode::from(code as u8),
+    None => ExitCode::from(255),
   }
 }
