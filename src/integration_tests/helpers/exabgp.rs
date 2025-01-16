813661f401086c5e315cc58f6230a3373b43e643
Eric Long <i@hack3r.moe>
2025-01-16 15:57:22 +0800

    tests: allow specify FLOW_EXABGP_PATH

--- src/integration_tests/helpers/exabgp.rs
+++ src/integration_tests/helpers/exabgp.rs
@@ -1,11 +1,21 @@
 use nix::unistd::{Uid, User};
+use std::borrow::Cow;
+use std::env;
+use std::ffi::OsStr;
 use std::path::Path;
 use std::process::Stdio;
+use std::sync::LazyLock;
 use tokio::io::{AsyncBufReadExt, BufReader};
 use tokio::process::{Child, Command};
 
+static EXABGP_PATH: LazyLock<Cow<'static, OsStr>> = LazyLock::new(|| {
+  env::var_os("FLOW_EXABGP_PATH")
+    .map(Cow::Owned)
+    .unwrap_or(Cow::Borrowed("exabgp".as_ref()))
+});
+
 pub async fn run_exabgp(config_path: impl AsRef<Path>, port: u16) -> anyhow::Result<Child> {
-  let mut exabgp = Command::new("exabgp")
+  let mut exabgp = Command::new(&*EXABGP_PATH)
     .arg(config_path.as_ref())
     .env("exabgp.tcp.port", port.to_string())
     .env("exabgp.daemon.user", User::from_uid(Uid::effective())?.unwrap().name)
